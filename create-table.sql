cb35d003a7c8a6af79bc1eaea515a7ac4c6ed600
Angga Pratama Mufrihan <anggapratama.mufrihan@photoninfotech.net>
2019-04-01 17:03:06 +0700

    uses template from tutorial

--- create-table.sql
+++ create-table.sql
@@ -1,5 +1,7 @@
-create table [dbo].[User](
+create table [dbo].[Registration](
     id INT NOT NULL IDENTITY(1,1) PRIMARY KEY(id),
-    firstname VARCHAR(30),
-    lastname VARCHAR(30)
+    name VARCHAR(30),
+    email VARCHAR(30),
+    job VARCHAR(30),
+    date DATE
 );
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
  <head>
- <Title>User Form</Title>
+ <Title>Registration Form</Title>
  <style type="text/css">
  	body { background-color: #fff; border-top: solid 10px #000;
  	    color: #333; font-size: .85em; margin: 20; padding: 20;
@@ -16,11 +16,12 @@
  </style>
  </head>
  <body>
- <h1>Tambahkan nama!</h1>
- <p>Tulis nama depan dan belakang, lalu klik <strong>Submit</strong> untuk menambahkan.</p>
+ <h1>Register here!</h1>
+ <p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
  <form method="post" action="index.php" enctype="multipart/form-data" >
-       Firstname  <input type="text" name="firstname" id="firstname"/></br></br>
-       Lastname <input type="text" name="lastname" id="lastname"/></br></br>
+       Name  <input type="text" name="name" id="name"/></br></br>
+       Email <input type="text" name="email" id="email"/></br></br>
+       Job <input type="text" name="job" id="job"/></br></br>
        <input type="submit" name="submit" value="Submit" />
        <input type="submit" name="load_data" value="Load Data" />
  </form>
@@ -39,36 +40,45 @@
 
     if (isset($_POST['submit'])) {
         try {
-            $firstname = $_POST['firstname'];
-            $lastname = $_POST['lastname'];
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $job = $_POST['job'];
+            $date = date("Y-m-d");
             // Insert data
-            $sql_insert = "INSERT INTO [dbo].[Users] (firstname, lastname) VALUES (?,?)";
+            $sql_insert = "INSERT INTO Registration (name, email, job, date) 
+                        VALUES (?,?,?,?)";
             $stmt = $conn->prepare($sql_insert);
-            $stmt->bindValue(1, $lastname);
-            $stmt->bindValue(2, $lastname);
+            $stmt->bindValue(1, $name);
+            $stmt->bindValue(2, $email);
+            $stmt->bindValue(3, $job);
+            $stmt->bindValue(4, $date);
             $stmt->execute();
         } catch(Exception $e) {
             echo "Failed: " . $e;
         }
 
-        echo "<h3>Berhasil ditambahkan!</h3>";
+        echo "<h3>Your're registered!</h3>";
     } else if (isset($_POST['load_data'])) {
         try {
-            $sql_select = "SELECT * FROM [dbo].[Users]";
+            $sql_select = "SELECT * FROM Registration";
             $stmt = $conn->query($sql_select);
             $registrants = $stmt->fetchAll(); 
             if(count($registrants) > 0) {
-                echo "<h2>Nama yang telah ditambahkan:</h2>";
+                echo "<h2>People who are registered:</h2>";
                 echo "<table>";
-                echo "<tr><th>Firstname</th>";
-                echo "<th>Lastname</th>";
+                echo "<tr><th>Name</th>";
+                echo "<th>Email</th>";
+                echo "<th>Job</th>";
+                echo "<th>Date</th></tr>";
                 foreach($registrants as $registrant) {
-                    echo "<tr><td>".$registrant['firstname']."</td>";
-                    echo "<td>".$registrant['lastname']."</td>";
+                    echo "<tr><td>".$registrant['name']."</td>";
+                    echo "<td>".$registrant['email']."</td>";
+                    echo "<td>".$registrant['job']."</td>";
+                    echo "<td>".$registrant['date']."</td></tr>";
                 }
                 echo "</table>";
             } else {
-                echo "<h3>Belum ada yang ditambahkan</h3>";
+                echo "<h3>No one is currently registered.</h3>";
             }
         } catch(Exception $e) {
             echo "Failed: " . $e;
